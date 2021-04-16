01d366686f569b163e2a29ff00092bed88e34190
kisaragi99 <58854608+kisaragi99@users.noreply.github.com>
2021-04-16 20:43:44 +0300

    resurrected succesfully

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import { Button, Container, Grid, TextField, useMediaQuery } from '@material-ui/core';
+import { useMediaQuery } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import DoneIcon from '@material-ui/icons/Done';
 import React, { useContext, useState } from 'react';
@@ -9,32 +9,35 @@ import Loader from './Loader';
 import firebase from "firebase";
 import s from './Chat.module.css';
 import ReactScrollableFeed from 'react-scrollable-feed';
+import { useForm } from "react-hook-form";
 
 
 const Chat = () =>{
   
-  const mediaMobile = useMediaQuery("(max-width: 768px)")
+  const mediaMobile = useMediaQuery("(max-width: 768px)");
+  const { register, handleSubmit, reset } = useForm();
   
   const {auth, firestore} = useContext(Context);
   const [user] = useAuthState(auth);
-  const [value, setValue] = useState('');
   const [messages, loading] = useCollectionData(
       firestore.collection('messages').orderBy('createdAt'), {idField: "id"}
   );
 
-  const sendMessage = async () => {
+  const sendMessage = async (data) => {
       firestore.collection('messages').add({
         uid: user.uid,
         displayName: user.displayName,
         photoURL: user.photoURL,
-        body: value,
+        body: data.messageBody,
         createdAt: firebase.firestore.FieldValue.serverTimestamp()
       })
-      setValue('');
+      reset();
   };
   
-  // firestore.collection('messages').doc(message.id).delete(); - If u want to delete a message(document).
+  // firestore.collection('messages').doc(message.id).delete(); - If u want to delete a message(document)
   const [deleteMessageId, setDeleteMessageId] = useState('');
+
+
 
   if(loading) {
     return <Loader/>
@@ -45,12 +48,8 @@ const Chat = () =>{
   }
 console.log('render', deleteMessageId)
   return <>
-    <Container maxWidth="md" className={s.mainContainer} >
-      <Grid container
-            justify="center"
-            style={mediaMobile ? {height: window.innerHeight -108, marginTop: 48, width: '100%' } : {height: window.innerHeight -63, marginTop: 62 } }
-            spacing = {mediaMobile ? 0 : 0 }
-            >
+    <div className={s.mainContainer} >
+      <div className={s.secondMainContainer}>
             
               <div className={s.innerContainer}>
               <ReactScrollableFeed>
@@ -84,32 +83,24 @@ console.log('render', deleteMessageId)
                 })}
                 </ReactScrollableFeed>
               </div>
-              
 
-              <Grid
-                    container
-                    direction={"column"}
-                    alignItems={"flex-end"}
-                    className={s.textFieldGrid}
-                    style={mediaMobile ? {width: '100%', marginTop: '0'} : {width: '80%', marginTop: '20px'}}
-                    spacing = {0}>
+              <form onSubmit={handleSubmit(sendMessage)} className={s.formInputWrapper}>
+                  <input 
+                  placeholder="type here" 
+                  {...register("messageBody",{ required: true, maxLength: 900 })} 
+                  className={s.messageInput} 
+                  autoComplete="off" 
+                  />
+                  <input value="send" type="submit" className={s.messageSendButton}></input>
+              </form>
 
-                      <TextField fullWidth 
-                                 variant={"outlined"} 
-                                 rowsMax={2} 
-                                 value={value} 
-                                 onChange={e => setValue(e.target.value)} 
-                                 placeholder={"type here"}
-                                 inputProps={{ maxLength: 900 }}
-                                 style={{borderRadius: "25px"}}
-                                 spacing ={0}
-                      />
-                      {value.length > 0 ? <Button variant={"outlined"} onClick={sendMessage}>Send</Button> : null}
-                      
-                  </Grid>
+
+
+
+
                   
-      </Grid>
-    </Container>
+      </div>
+    </div>
   </>
 };
 
